575d2514cff4a05b155194790649cb49e8d544e4
Trần Đình Thịnh <156297768+thinhdabezt@users.noreply.github.com>
2025-09-21 17:40:21 +0700

    Refactor authentication flow and add logout functionality in NotesView

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as devtools;
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -28,27 +30,96 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
-        ),
-        builder: (context, asyncSnapshot) {
-          switch (asyncSnapshot.connectionState) {
-            case ConnectionState.done:
-              final user = FirebaseAuth.instance.currentUser;
-              if (user != null) {
-                if (user?.emailVerified ?? false) {
-                  print("User is verified");
-                } else {
-                  return const VerifyEmailView();
-                }
+      future: Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      ),
+      builder: (context, asyncSnapshot) {
+        switch (asyncSnapshot.connectionState) {
+          case ConnectionState.done:
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                return const NotesView();
               } else {
-                return const CircularProgressIndicator();
+                return const VerifyEmailView();
               }
-              return const CircularProgressIndicator();
-            default:
-              return const CircularProgressIndicator();
-          }
-        },
-      );
+            } else {
+              return const LoginView();
+            }
+          default:
+            return const NotesView();
+        }
+      },
+    );
   }
+}
+
+enum MenuAction { logout }
+
+class NotesView extends StatefulWidget {
+  const NotesView({super.key});
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Your Note(s)"),
+        backgroundColor: Colors.blueAccent,
+        actions: [
+          PopupMenuButton(
+            onSelected: (value) async {
+              switch(value){
+                case MenuAction.logout:
+                  final signOut = await showLogOutDialog(context);
+                  if(signOut){
+                    await FirebaseAuth.instance.signOut();
+                    Navigator.of(context).pushNamedAndRemoveUntil("/login/", (route) => false);
+                  }
+              }
+            },
+            itemBuilder: (context) {
+              return const [
+                PopupMenuItem<MenuAction>(
+                  value: MenuAction.logout,
+                  child: Text("Sign Out"),
+                ),
+              ];
+            },
+          ),
+        ],
+      ),
+      body: const Center(child: Text("Hello World")),
+    );
+  }
+}
+
+Future<bool> showLogOutDialog(BuildContext context){
+  return showDialog<bool>(
+    context: context, 
+    builder: (context){
+      return AlertDialog(
+        title: const Text("Sign Out"),
+        content: const Text("Are you sure you want to sign out?"),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(false);
+            }, 
+            child: const Text("Cancel")
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(true);
+            }, 
+            child: const Text("Sign Out")
+          ),
+        ],
+      );
+    }
+  ).then((value) => value ?? false);
 }
